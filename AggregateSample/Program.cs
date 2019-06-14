ac51bee57c7c43923576a128d9ed02a41d6b357a
mmejidana <mmejidana@BAIDEVLP103>
2019-06-14 14:58:01 +0800

    Add project files.

---
+++ AggregateSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AggregateSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //without linq aggregate
+            string[] countries = { "Philippines", "US", "Canada", "France"};
+            string result = string.Empty;
+
+            foreach(string str in countries)
+            {
+                result = result + str + ", ";
+            }
+
+            Console.WriteLine(result);
+
+            //with linq aggregate
+            string result1 = countries.Aggregate( (a,b) => a + ", " + b);
+            Console.WriteLine(result1);
+        }
+    }
+}
